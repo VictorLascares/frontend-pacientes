3c577c82a8c1f10ef9453395b016fe9630e03ff2
VictorLascares <vmlascares@gmail.com>
2022-11-30 12:53:57 -0600

    Creando formulario para Pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,87 @@
-
 const Formulario = () => {
   return (
-    <div>
-      Formulario
-    </div>
-  )
+    <>
+      <p className="text-lg text-center mb-10">
+        Añade tus pacientes y{" "}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form className="bg-white py-10 px-5 shadow-md rounded-md">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            type="text"
+            id="mascota"
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 focus:outline-none"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="text-gray-700 uppercase font-bold"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            type="text"
+            id="propietario"
+            placeholder="Nombre del dueño de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 focus:outline-none"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="text-gray-700 uppercase font-bold">
+            Correo Electronico
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="ejemplo@correo.com"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 focus:outline-none"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="fecha" className="text-gray-700 uppercase font-bold">
+            Fecha de Alta
+          </label>
+          <input
+            type="date"
+            id="fecha"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 focus:outline-none"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="text-gray-700 uppercase font-bold"
+          >
+            Sintomas
+          </label>
+          <textarea
+            id="sintomas"
+            rows="5"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 focus:outline-none resize-none"
+          ></textarea>
+        </div>
+
+        <input
+          type="submit"
+          value="Agregar Paciente"
+          className="bg-indigo-700 hover:bg-indigo-500 w-full p-3 text-white transition-all cursor-pointer"
+        />
+      </form>
+    </>
+  );
 };
 
 export default Formulario;
--- src/pages/ManagePatients.jsx
+++ src/pages/ManagePatients.jsx
@@ -3,7 +3,7 @@ import PatientsList from "../components/PatientsList";
 
 const ManagePatients = () => {
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-col md:flex-row gap-10">
       <div className="md:w-1/2 lg:w-2/5">
         <Formulario />
       </div>
